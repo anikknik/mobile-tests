ddbceb5fda74bd5c3e7ce37c26666dec061f2db9
anikknik <anikknik@gmail.com>
2022-06-20 00:46:42 +0300

    added name and pass properties

--- src/test/java/ru/evo/helpers/Browserstack.java
+++ src/test/java/ru/evo/helpers/Browserstack.java
@@ -1,15 +1,19 @@
 package ru.evo.helpers;
 
+import org.aeonbits.owner.ConfigFactory;
+import owner.BrowserstackOwner;
+
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 
 public class Browserstack {
+    static BrowserstackOwner bsOwner = ConfigFactory.create(BrowserstackOwner.class);
 
     public static String videoUrl(String sessionId) {
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
-                .auth().basic("bsuser_5sdabT", "FiysMZVvjPtcmGH1dShu")
+                .auth().basic(bsOwner.name(), bsOwner.access_key())
                 .log().all()
                 .when()
                 .get(url)
